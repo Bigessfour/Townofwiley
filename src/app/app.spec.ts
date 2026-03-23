ae88d88bbeb887c4025280679fb5c6f11337837d
bigessfour <bigessfour@gmail.com>
2026-03-22 19:37:13 -0600

    Apply trunk formatting for ESLint setup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,8 +1,8 @@
 import { provideHttpClient } from '@angular/common/http';
 import {
-  HttpTestingController,
-  TestRequest,
-  provideHttpClientTesting,
+    HttpTestingController,
+    TestRequest,
+    provideHttpClientTesting,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
